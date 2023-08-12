8899e933c419916f3b1cd74768b0d626e59acab1
MauricioNC <mauricionc19@gmail.com>
2023-08-12 13:43:23 -0600

    Models associations completed and bcrypt gem installed.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  belongs_to :user
+  belongs_to :director
+  has_many :movies_genres
+  has_many :movies_genres, through: :movies_genres
 end
--- app/models/movies_genre.rb
+++ app/models/movies_genre.rb
@@ -1,4 +1,6 @@
 class MoviesGenre < ApplicationRecord
   belongs_to :movie
   belongs_to :genre
+  has_many :movies_genres
+  has_many :genres, through: :movies_genres
 end
