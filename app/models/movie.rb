43a611869f453f0c82f17e588c5b9a028fe9971a
MauricioNC <mauricionc19@gmail.com>
2023-08-15 22:59:36 -0600

    Comments polymorphic
    1. Was removed the 'movie_id' field recntly created from Comments model.
    2. Was created a new migration to add a commentable field
       ("commentable_type" and "commentable_id") to Comments model.
    3. Was added the corresponding associations between comments and movies.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,9 @@
 class Movie < ApplicationRecord
   belongs_to :user
   belongs_to :director
+  
   has_many :movies_genres, dependent: :destroy
   has_many :genres, through: :movies_genres, dependent: :destroy
+
+  has_many :comments, as: :commentable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_16_043222) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_16_045123) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +20,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_16_043222) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.bigint "movie_id", null: false
+    t.string "commentable_type", null: false
+    t.bigint "commentable_id", null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
